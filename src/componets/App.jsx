a174d8941869dab2cb7c720298743f5a6b0ab176
Manish Kumar Sahu <92624581+manish-3012@users.noreply.github.com>
2023-03-11 10:17:45 +0530

    Initial commit

---
+++ src/componets/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Heading from "./componets/Heading";
+
+const date = new Date();
+const currentTime = date.getHours();
+
+let greeting;
+
+const customStyle = {
+  color: ""
+};
+
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good Night";
+  customStyle.color = "blue";
+}
+
+return;
