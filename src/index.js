b37a0515551d0f2fe9a4366a9d533d17b6b3c5e4
Manish Kumar Sahu <sahum6703@gmail.com>
2023-03-11 05:07:14 +0000

    Update 5 files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./componets/App";
-import Heading from "./componets/Heading";
+import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
